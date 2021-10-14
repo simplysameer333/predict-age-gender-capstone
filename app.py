9277b825115d9192acfb72bfe2c6bc8f9851def7
Sameer Hameed <samorsameer@gmail.com>
2021-10-14 02:54:24 +0100

    Removing unused code

--- app.py
+++ app.py
@@ -39,33 +39,29 @@ print(xgb_best_age_group_model)
 
 """## **4.2 Best Test data from S3**"""
 
+# Fetch data to be predicted (Test Data)
 data_path = 'test_sample/'
-
 test_data_50_sample_path = 'test_data_50_sample.npz'
-test_50_device_ids_path = 'test_50_device_ids.csv'
-
-s3.Bucket(BUCKET_NAME).download_file(data_path+test_50_device_ids_path, test_50_device_ids_path)
 s3.Bucket(BUCKET_NAME).download_file(data_path+test_data_50_sample_path, test_data_50_sample_path)
-
 test_data_50_sample = sparse.load_npz(test_data_50_sample_path)
-test_50_device_id_df = pd.read_csv(test_50_device_ids_path, encoding='utf-8', dtype={'app_id': 'string'} )
-
 
 @app.route('/')
 def home():
     return 'Hello World'
 
-
 @app.route('/predict_gender_age', methods=['GET'])
 def predict_gender():
-	xgb_best_gender_model_prediction = xgb_best_gender_model.predict_proba(test_data_50_sample)
-	xgb_best_gender_model_prediction_df = pd.DataFrame(data=xgb_best_gender_model_prediction, columns=["M", "F"])
-	device_ids_gender_prbabilities_df = pd.concat([test_50_device_id_df, xgb_best_gender_model_prediction_df], axis = 1)
+	# gender probabilities
+	xgb_best_gender_prediction = xgb_best_gender_model.predict_proba(test_data_50_sample)
+	xgb_best_gender_prediction_df = pd.DataFrame(data=xgb_best_gender_prediction, columns=["M", "F"])
+	
+	# age probabilities
 	xgb_best_age_group_prediction = xgb_best_age_group_model.predict_proba(test_data_50_sample)
+	xgb_best_age_group_prediction_df = pd.DataFrame(data=xgb_best_age_group_prediction, columns=["0-24", "25-32", "32+"])
 
-	xgb_best_gender_model_prediction_df = pd.DataFrame(data=xgb_best_age_group_prediction, columns=["0-24", "25-32", "32+"])
-	device_ids_gender_prbabilities_df = pd.concat([device_ids_gender_prbabilities_df, xgb_best_gender_model_prediction_df], axis = 1)
-	return jsonify(device_ids_gender_prbabilities_df.to_json())  
+	#merge gender and age
+	gender_age_group_prbabilities_df = pd.concat([xgb_best_gender_prediction_df, xgb_best_age_group_prediction_df], axis = 1)
+	return jsonify(gender_age_group_prbabilities_df.to_json())  
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
